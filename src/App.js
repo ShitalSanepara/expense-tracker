70b5ce06efe310e9883433595b1b6b46ba962cae
ShitalSanepara <shitalsanepara@gmail.com>
2024-01-05 18:34:08 +0530

    Adding From

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import AddFrom from './Components/AddFrom';
+import Table from './Components/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+      <Route index element={<AddFrom />} />
+      <Route path="table" element={<Table />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/AddFrom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddFrom() {
+  return (
+    <div>
+      <h1>shital</h1>
+    </div>
+  )
+}
